200b9c75b9640c0622ad672b666c0f0936883c23
osmanamjad <50644787+osmanamjad@users.noreply.github.com>
2019-10-09 20:22:57 -0400

    Fixed some formatting issues

--- determinants.c
+++ determinants.c
@@ -3,7 +3,6 @@
 #include<string.h>
 #include<math.h>
 
-#define MAX_INT_LEN 1024
 int** remove_column(int dim, int column, int** array3)
 {
     int** small = malloc(dim*sizeof(int*));
@@ -11,43 +10,32 @@ int** remove_column(int dim, int column, int** array3)
     {
         small[i] = malloc(dim*sizeof(int));
     }
-	for(int i=0;i<dim-1;i++)
-	{
-	    int j;
-		for(j=0;j<column;j++)
-		{
-			small[i][j] = array3[i+1][j];
-		}
-		for(j=column;j<dim-1;j++)
-        {
-            small[i][j] = array3[i+1][j+1];
-		}
-	}
-	return small;
+	
 }
 int find_determinant(int dim, int** array2)
 {
-	int result = 0;
-	if(dim==2)
-	{
-		 return (array2[0][0] * array2[1][1]) - (array2[0][1] * array2[1][0]);
+    int result = 0;
+    if(dim==2)
+    {
+        return (array2[0][0] * array2[1][1]) - (array2[0][1] * array2[1][0]);
+    }
+    else
+    {
+        int j;
+        for(j=0;j<dim;j++)
+        {
+       	    result += pow(-1, j) * array2[0][j] * find_determinant(dim-1, remove_column(dim, j, array2));
 	}
-	else
-	{
-	    int j;
-		for(j=0;j<dim;j++)
-		{
-			result += pow(-1, j) * array2[0][j] * find_determinant(dim-1, remove_column(dim, j, array2));
-		}
-	}
-	free(*array2);
-	free(array2);
-	return result;
+    }
+    free(*array2);
+    free(array2);
+    return result;
 }
+
 int** get_matrix(int dim)
 {
-	int i;
-	int j;
+    int i;
+    int j;
     int** array = malloc(dim*sizeof(int*));
     for(i=0;i<dim;i++)
     {
@@ -71,14 +59,14 @@ int** get_matrix(int dim)
         }
         printf("\n");
     }
-	return array;
+    return array;
 }
+
 int main()
 {
-	int dim;
-	printf("Please enter the number of columns/rows in the matrix. It must be a square matrix.\n");
-	scanf("%d", &dim);
-	getchar();
-	printf("%d", find_determinant(dim, get_matrix(dim)));
-
+    int dim;
+    printf("Please enter the number of columns/rows in the matrix. It must be a square matrix.\n");
+    scanf("%d", &dim);
+    getchar();
+    printf("%d", find_determinant(dim, get_matrix(dim)));
 }
