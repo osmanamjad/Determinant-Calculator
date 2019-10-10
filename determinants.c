a96b992169af1e9f72f861ef4d5d2b32f48f2388
osmanamjad <50644787+osmanamjad@users.noreply.github.com>
2019-10-09 20:16:35 -0400

    Fixed some compilation and syntax errors

--- determinants.c
+++ determinants.c
@@ -1,76 +1,84 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<math.h>
 
 #define MAX_INT_LEN 1024
-int* removing(int dim, int* p, int row, int column)
+int** remove_column(int dim, int column, int** array3)
 {
-	int new[dim-1][dim-1];
-	int i;
-	int j;
-	for(i=0;i<row;i++)
+    int** small = malloc(dim*sizeof(int*));
+    for(int i=0;i<dim;i++)
+    {
+        small[i] = malloc(dim*sizeof(int));
+    }
+	for(int i=0;i<dim-1;i++)
 	{
+	    int j;
 		for(j=0;j<column;j++)
 		{
-			new[i][j]
+			small[i][j] = array3[i+1][j];
+		}
+		for(j=column;j<dim-1;j++)
+        {
+            small[i][j] = array3[i+1][j+1];
 		}
 	}
+	return small;
 }
-int determinant_finder(int dim, int* p)
+int find_determinant(int dim, int** array2)
 {
-	int result;
-	int i;
-	int j;
+	int result = 0;
 	if(dim==2)
 	{
-		 result = *(p+(0*dim)+0) * *(p+(1*dim)+1) - (*(p+(dim*0)+1) * *(p+(dim*1)+0));
-		 return result;
+		 return (array2[0][0] * array2[1][1]) - (array2[0][1] * array2[1][0]);
 	}
 	else
 	{
+	    int j;
 		for(j=0;j<dim;j++)
 		{
-			determinant_finder(dim-1, p, 0, j); 
+			result += pow(-1, j) * array2[0][j] * find_determinant(dim-1, remove_column(dim, j, array2));
 		}
 	}
+	free(*array2);
+	free(array2);
+	return result;
 }
-int get_matrix(int dim, int depth)
+int** get_matrix(int dim)
 {
 	int i;
 	int j;
-	int array[dim][dim];
-	int result;
-	if(depth == 0)
-	{
-		for(i=0;i<dim;i++)
-		{
-			for(j=0;j<dim;j++)
-			{
-				printf("Please enter the entry at index(%d, %d)\n", i, j);
-				scanf("%d", &array[i][j]);
-				getchar();
-			}
-		}
-		printf("Your matrix is:\n");
-		for(i=0;i<dim;i++)
-		{
-			for(j=0;j<dim;j++)
-			{
-				printf("%d ", array[i][j]);
-			}
-			printf("\n");
-		}
-	}
-	
+    int** array = malloc(dim*sizeof(int*));
+    for(i=0;i<dim;i++)
+    {
+        array[i] = malloc(dim*sizeof(int));
+    }
+    for(i=0;i<dim;i++)
+    {
+        for(j=0;j<dim;j++)
+        {
+            printf("Please enter the entry at index(%d, %d)\n", i, j);
+            scanf("%d", &array[i][j]);
+            getchar();
+        }
+    }
+    printf("Your matrix is:\n");
+    for(i=0;i<dim;i++)
+    {
+        for(j=0;j<dim;j++)
+        {
+            printf("%d ", array[i][j]);
+        }
+        printf("\n");
+    }
+	return array;
 }
 int main()
 {
-	//int array[3][3] = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
 	int dim;
 	printf("Please enter the number of columns/rows in the matrix. It must be a square matrix.\n");
 	scanf("%d", &dim);
 	getchar();
-	get_matrix(dim);
-	
-	
+	printf("%d", find_determinant(dim, get_matrix(dim)));
+
 }
